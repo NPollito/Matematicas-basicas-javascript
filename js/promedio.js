cdf4d0ef934bd09167128241b0f40b0f159392b9
nilton pv <cruzito@DESKTOP-A7T8MKK.localdomain>
2021-07-17 18:31:17 -0500

    taller 3, termino de calc media y agregando img

--- js/promedio.js
+++ js/promedio.js
@@ -1,11 +1,6 @@
-const promedioAritmetica = (lista) => lista.reduce((a, b) => a + b) / lista.length;  //promedio
+const promedioAritmetica = lista => lista.reduce((a, b) => a + b) / lista.length;  //promedio
 
-const lista1 = [120, 45, 50, 250, 300];
-const ordenarLista = lista1.sort((a,b) => a - b);
-
-const mitadLista = parseInt(ordenarLista.length / 2);
-
-function esPar(numero) {
+function esPar(numero) {     //Es par o impar
     if(numero % 2 === 0) {
         return true;
     }else {
@@ -13,18 +8,73 @@ function esPar(numero) {
     }
 };
 
-//Es par
+// Promedio
 
-let mediana;
+function calPromedio() {
 
-if(esPar(ordenarLista.length)) {
-    const elemento1 = ordenarLista[mitadLista - 1];
-    const elemento2 = ordenarLista[mitadLista];
+    const botPromedio = document.getElementById("botPromedio");
+    const texto = document.getElementById("texto");
 
-    const calc = promedioAritmetica([elemento1, elemento2]);
-
-    mediana = calc;
-
-}else {
-    mediana = ordenarLista[mitadLista];
+    const inputPromedio = document.getElementById("inputPromedio").value;
+    const separaArray = Array.from(inputPromedio.split(','),Number);
+    
+    // const parsePromedio = parseInt(inputPromedio);
+    const calcPromedio = promedioAritmetica(separaArray).toFixed(2);
+    texto.innerText = `El promedio es: ${calcPromedio}`
+    
 }
+
+
+// Mediana
+
+function calMedia() {
+
+    const botMedia = document.getElementById("botMedia");
+    const texto = document.getElementById("texto");
+
+    const inputMedia = document.getElementById("inputMedia").value;
+    const separaArray = Array.from(inputMedia.split(','),Number);
+    const ordenarArray = separaArray.sort((a,b) => a - b);
+
+    const mitadArray = parseInt(ordenarArray.length / 2);
+
+    let mediana;
+
+    if(esPar(ordenarArray.length)) {
+        const elemento1 = ordenarArray[mitadArray - 1];
+        const elemento2 = ordenarArray[mitadArray];
+
+        const calc = promedioAritmetica([elemento1, elemento2]);
+
+        mediana = calc;
+
+    }else {
+        mediana = ordenarArray[mitadArray];
+    }
+
+    texto.innerText = `La media es: ${mediana}`
+
+}
+
+// const lista1 = [120, 45, 50, 250, 300];
+// const ordenarLista = lista1.sort((a,b) => a - b);
+
+// const mitadLista = parseInt(ordenarLista.length / 2);
+
+
+
+// //Es par
+
+// let mediana;
+
+// if(esPar(ordenarLista.length)) {
+//     const elemento1 = ordenarLista[mitadLista - 1];
+//     const elemento2 = ordenarLista[mitadLista];
+
+//     const calc = promedioAritmetica([elemento1, elemento2]);
+
+//     mediana = calc;
+
+// }else {
+//     mediana = ordenarLista[mitadLista];
+// }
